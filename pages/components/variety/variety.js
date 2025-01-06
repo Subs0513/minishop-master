07abeea4a4b66e4a5b0287962be8b0e92c68f128
Subs0513 <subs0514@gmail.com>
2025-01-06 20:25:46 +0800

    1.0.4
    稳定版，variety中的按钮可能有问题

--- pages/components/variety/variety.js
+++ pages/components/variety/variety.js
@@ -1,12 +1,18 @@
 Page({
   data: {
     navList: [
+      { id: 0, name: '热销商品' },
       { id: 1, name: '饼干糕点' },
       { id: 2, name: '糖果坚果' },
       { id: 3, name: '休闲零食' },
       { id: 4, name: '酒水饮料' },
     ],
     varietyList: [
+      [
+        { name: "郭朝阳", price: 9999, num: 1, id: 999 },
+        { name: "杜璟钰", price: 9.9, num: 1, id: 888 },
+        { name: "张正澍", price: 888, num: 1, id: 777 },
+      ],
       [
         { name: "巧克力饼干", price: 12.5, num: 100, id: 1 },
         { name: "奶油蛋卷", price: 15, num: 50, id: 2 },
@@ -21,7 +27,8 @@ Page({
         { name: "曲奇礼盒", price: 35, num: 20, id: 11 },
         { name: "椰蓉蛋糕", price: 22, num: 30, id: 12 },
         { name: "柠檬曲奇", price: 15.8, num: 70, id: 13 },
-        { name: "巧克力布朗尼", price: 26, num: 30, id: 14 }
+        { name: "巧克力布朗尼", price: 26, num: 30, id: 14 },
+        { name: " ", price:  0, num:  0, id:0   }
       ],
       [
         { name: "果味软糖", price: 8.8, num: 200, id: 15 },
@@ -36,7 +43,9 @@ Page({
         { name: "薄荷巧克力", price: 21, num: 50, id: 24 },
         { name: "红枣夹核桃", price: 22, num: 50, id: 25 },
         { name: "咖啡豆巧克力", price: 29, num: 50, id: 26 },
-        { name: "草莓牛奶糖", price: 12.5, num: 100, id: 27 }
+        { name: "草莓牛奶糖", price: 12.5, num: 100, id: 27 },
+        { name: " ", price:  0, num:  0, id:0   }
+
       ],
       [
         { name: "海苔脆片", price: 10, num: 150, id: 28 },
@@ -50,7 +59,8 @@ Page({
         { name: "香蕉片", price: 6.5, num: 120, id: 36 },
         { name: "蒜香青豆", price: 7.5, num: 150, id: 37 },
         { name: "芒果干", price: 19.8, num: 60, id: 38 },
-        { name: "芝麻糊", price: 8, num: 90, id: 39 }
+        { name: "芝麻糊", price: 8, num: 90, id: 39 },
+        { name: " ", price:  0, num:  0, id:0   }
       ],
       [
         { name: "可乐", price: 5, num: 500, id: 40 },
@@ -61,15 +71,31 @@ Page({
         { name: "金桔柠檬汁", price: 9, num: 100, id: 45 },
         { name: "蜂蜜柠檬茶", price: 10, num: 80, id: 46 },
         { name: "果汁棒冰", price: 5.5, num: 200, id: 47 },
-        { name: "姜味苏打水", price: 6, num: 150, id: 48 }
+        { name: "姜味苏打水", price: 6, num: 150, id: 48 },
+        { name: " ", price:  0, num:  0, id:0   }
       ]
     ], 
     curIndex: 0,  // 当前选中的分类
     cartTotal: 0, // 选购的商品总数
     cartList: [], // 购物车商品列表
-    cartVisible: false, // 购物车显示状态
+    iconType: 'circle',  // 初始状态是circle
+    cartVisible: true, // 购物车显示状态
   },
-
+    // 处理点击事件，切换图标
+  toggleIcon: function() {
+    // 获取被点击的图标的索引
+    const index = e.currentTarget.dataset.index;
+    
+    // 根据点击的图标索引来切换其状态
+    let newIconTypes = this.data.iconTypes;
+    newIconTypes[index] = newIconTypes[index] === 'circle' ? 'success' : 'circle';
+    
+    this.setData({
+      iconTypes: newIconTypes
+    });
+  
+  },
+  
   selectNav(e) {
     this.setData({
       curIndex: e.currentTarget.dataset.index
